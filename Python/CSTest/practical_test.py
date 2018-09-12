502e155465e46566d06e8d048bef98d78024abb5
Felipe Valencia <>
2018-09-12 16:25:21 -0500

    CSTEST

---
+++ Python/CSTest/practical_test.py
@@ -0,0 +1,25 @@
+print("Welcome!")
+name = input("What is your name?\n")
+tutor_group = input("What is your tutor group?\n")
+print("Lets start with your grades")
+obj1 = str("\nTo develop a knowledge and understanding of "+input("Enter the objective name\n"))
+grade11 = int(input("Enter the first grade of the objective\t"))
+grade12 = int(input("Enter the second grade of the objective\t"))
+grade13 = int(input("Enter the third grade of the objective\t"))
+print("\n\tNow Objective 2:\n")
+
+obj2 = str("\nTo develop a knowledge and understanding of "+input("Enter the objective name\n"))
+grade21 = int(input("Enter the first grade of the objective\t"))
+grade22 = int(input("Enter the second grade of the objective\t"))
+grade23 = int(input("Enter the third grade of the objective\t"))
+
+grade1 = [grade11,grade12,grade13]
+sorted1 = sorted(grade1)
+highest1 = sorted1[len(grade1)-1]
+grade2 = [grade21,grade22,grade23]
+sorted2 = sorted(grade2)
+highest2 =sorted2[len(grade2)-1]
+average1 = sum(grade1) / float(len(grade1))
+average2 = sum(grade2) / float(len(grade2))
+totalavg = average2+average1 / 2
+print("{0}'s total average is {1}, their average for Objective 1 was {2} and their highest grade was {3}\nFor Objective two their average {4} and highest grade was {5}".format(name,totalavg,average1,highest1,average2,highest2))
