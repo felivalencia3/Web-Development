2e02251dfaaa43dff9f2542f23aaf06d3cd27a94
Felipe Valencia <>
2018-12-13 18:05:34 -0500

    tensorflow AI

--- Python/BattleShip/battleship.py
+++ Python/BattleShip/battleship.py
@@ -1,21 +1,96 @@
-import time
-
+from time import sleep
 numRow = 6
 numCol = 6
 board1 = [[False for column1 in range(numCol)] for row1 in range(numRow)]
 board2 = [[False for column2 in range(numCol)] for row2 in range(numRow)]
 
 
-def setShip1(coords):
-    if len(coords) == 2:
-        coord1 = coords[0]
-        coord2 = coords[2]
-        board1[coord1][coord2]
+def setShip1(coord1,coord2):
+        coord1 = int(coord1)
+        coord2 = int(coord2)
+        board1[coord1][coord2] = True
 
-print("Welcome to Felipe's Battleship game\nDecide who will play first and pass the computer to them")
-print("Waiting for 5 seconds...")
-time.sleep(5)
-print("Alright Player 1, Let's start\n\n")
+def askForCoords():
+    row = input("\nEnter Row Number:\n")
+    col = input("\nEnter Column Number\n")
+    return {
+        "row": row,
+        "col": col
+    }
+
+def player2Start():
+    print("Let's start setting up your board")
+    print("First we will set up your 1 space spy-ship:\n")
+    for i in range(1):
+        coords = askForCoords()
+        row = coords["row"]
+        column = coords["col"]
+        setShip1(row, column)
+    print("Done.\n")
+    print("\nNow for your 2 space ship")
+    for k in range(2):
+        coords = askForCoords()
+        row = coords["row"]
+        column = coords["col"]
+        setShip1(row, column)
+    print("\n\nNow your 3 space ship")
+    for x in range(3):
+        coords = askForCoords()
+        row = coords["row"]
+        column = coords["col"]
+        setShip1(row, column)
+    print("\n\n4 space ship:")
+    for x in range(4):
+        coords = askForCoords()
+        row = coords["row"]
+        column = coords["col"]
+        setShip1(row, column)
+    print("\n\nAnd last but not least, the 5 Space Ship")
+    for x in range(5):
+        coords = askForCoords()
+        row = coords["row"]
+        column = coords["col"]
+        setShip1(row, column)
+    print("\n\n\n\n\n\tDone!")
+
+def player1Start():
+    print("Let's start setting up your board")
+    print("First we will set up your 1 space spy-ship:\n")
+    for i in range(1):
+        coords = askForCoords()
+        row = coords["row"]
+        column = coords["col"]
+        setShip1(row, column)
+    print("Done.\n")
+    print("\nNow for your 2 space ship")
+    for k in range(2):
+        coords = askForCoords()
+        row = coords["row"]
+        column = coords["col"]
+        setShip1(row, column)
+    print("\n\nNow your 3 space ship")
+    for x in range (3):
+        coords = askForCoords()
+        row = coords["row"]
+        column = coords["col"]
+        setShip1(row, column)
+    print("\n\n4 space ship:")
+    for x in range (4):
+        coords = askForCoords()
+        row = coords["row"]
+        column = coords["col"]
+        setShip1(row, column)
+    print("\n\nAnd last but not least, the 5 Space Ship")
+    for x in range (5):
+        coords = askForCoords()
+        row = coords["row"]
+        column = coords["col"]
+        setShip1(row, column)
+    print("\n\n\n\n\n\tDone!")
+
+
+print("Welcome to Felipe's Battleship game!")
+sleep(3)
 fakeTable = [['0,0', '0,2', '0,3', '0,4', '0,5', '0,6'],
              ['2,0', '2,2', '2,3', '2,4', '2,5', '2,6'],
              ['3,0', '3,2', '3,3', '3,4', '3,5', '3,6'],
@@ -27,19 +102,22 @@ for row in fakeTable:
 print("\nWhat you see above is the way that your board will be spread out\n\n\tYou will have 5 ships:\n")
 print("1 battleship, takes 5 spaces\n1 cruiser that takes up 4 spaces\n1 submarine that takes up 3 spaces")
 print("1 destroyer that takes 2 spaces\n1 spy ship that takes up 1 space")
-time.sleep(1)
+print("\n\nPlayer 1, take the computer and don't show your oponent")
+sleep(2)
 ready = input("Ready? [y/n]\n")
 if ready == "yes" or ready == "y":
-    print("Let's start setting up your board")
-    print("First we will set up your 1 space spy-ship:\n")
-    coordinates = input("Enter the coordinates for your ship. Example: 3,3 or 1,2\n(Type it with a comma)\n\t")
-    print(coordinates)
-    setShip(1,coordinates)
-    print(board1)
+    player1Start()
+    print("You have 4 seconds to pass the computer to the other player so that they can set up")
+    sleep(5)
+    player2Start()
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nLets Start!")
+    print("Player 1 will start you have 2 seconds to hand back the computer")
+    sleep(2)
+
+
 elif ready == "no" or ready == "n":
     print("Ok then")
     import sys
-
     sys.exit()
 
 else:
--- React.js /reactdocs/src/GuestGreet.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export default class GuestGreet extends React.Component {
-    render() {
-        return <h1>Please sign up.</h1>;
-    }
-}
